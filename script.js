5c04b0310818eb1cb63814f00367231f4d878cdb
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2025-12-02 12:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 class Animal {
-  public _species: string;
+    constructor(species) {
+        this._species = species;
+    }
 
-  constructor(species: string) {
-    this._species = species;
-  }
+    get species() {
+        return this._species;
+    }
 
-  get species(): string {
-    return this._species;
-  }
-
-  makeSound(): void {
-    console.log(`The ${this._species} makes a sound`);
-  }
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
 }
 
 class Cat extends Animal {
-  purr(): void {
-    console.log("purr");
-  }
+    purr() {
+        console.log("purr");
+    }
 }
 
 class Dog extends Animal {
-  bark(): void {
-    console.log("woof");
-  }
+    bark() {
+        console.log("woof");
+    }
 }
 
 // Do not change the code below this line
+// These lines make the classes globally available for the Cypress tests
 window.Animal = Animal;
 window.Cat = Cat;
 window.Dog = Dog;
